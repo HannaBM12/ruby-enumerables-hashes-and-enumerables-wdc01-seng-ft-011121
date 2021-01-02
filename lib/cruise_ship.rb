708c11dd073eacc77d8bb16017f808bd338fe131
Hanna Mulugeta <hannabmulugeta@gmail.com>
2021-01-02 23:13:27 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,7 +10,7 @@ def select_winner(passengers)
   # add the code snippet here!
   passengers.each { |key, value|
     if key == :suite_a && value[0] == "A"
-      value
+      puts value
     end
   }
 
